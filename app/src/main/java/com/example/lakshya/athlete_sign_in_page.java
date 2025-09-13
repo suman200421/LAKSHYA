9ee8acdf6dfe173984ce21bc7a39cb393091f2b1
suman200421 <padmanavakhamrui8479@gmail.com>
2025-09-13 12:40:21 +0530

    athlete sign in updated

--- app/src/main/java/com/example/lakshya/athlete_sign_in_page.java
+++ app/src/main/java/com/example/lakshya/athlete_sign_in_page.java
@@ -1,6 +1,10 @@
 package com.example.lakshya;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,26 @@ public class athlete_sign_in_page extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    // Java
+    Spinner mySpinner = findViewById(R.id.my_spinner);
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+            R.array.spinner_items, android.R.layout.simple_spinner_item);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    mySpinner.setAdapter(adapter);
+
+    mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+
+    {
+        public void onItemSelected (AdapterView < ? > parent, View view, int position, long id){
+        String selectedItem = parent.getItemAtPosition(position).toString();
+        // Handle selected item
+    }
+
+        @Override
+        public void onNothingSelected (AdapterView < ? > parent) {
+        // Handle no selection
+    }
+    });
     }
 }
